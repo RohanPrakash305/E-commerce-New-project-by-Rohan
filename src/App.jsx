799c762d3af68c3fa2af1096f49056f7d3afd1ab
RohanPrakash305 <atulraj844114@gmail.com>
2025-07-28 23:31:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Registration from './pages/Registration'
+import { Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+
+function App() {
+  return (
+      
+    <>
+      <Routes>
+        <Route path ='/' element={ <Home/>}/>
+         <Route path ='/login' element={ <Login/>}/>
+          <Route path ='/signup' element={ <Registration/>}/>
+      </Routes>
+    </>
+
+   )
+}
+
+export default App
